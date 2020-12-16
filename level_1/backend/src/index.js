2a996337d6eda84b9abb6c33908bb38ae6ff5e10
Bruno Montebello <brunormontebello@gmail.com>
2020-12-16 23:27:07 +0000

    Create query, params and body

--- level_1/backend/src/index.js
+++ level_1/backend/src/index.js
@@ -1,6 +1,7 @@
-const { request } = require('express');
+const { request, response } = require('express');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 /**
  * METODOS HTTP:
@@ -10,9 +11,18 @@ const app = express();
  * PUT: atualiza todos os dados de um recurso (EXEMPLO DE RECURSO:/projects)
  * DELETE: deletar infos no
  *
+ *
+ * TIPOS DE PARSMETROS
+ * query params: usado mais para filtros e paginacao.
+ * route params: identificar recursos p deletar ou atualizar
+ * request body: conteudo de criar e editar recurso (JSON)
  */
 
 app.get('/projects', (request, response) => {
+  const query = request.query;
+
+  console.log(query);
+
   return response.json([
     'Primeiro freella do Brunao',
     'Segundo freella do Brunao'
@@ -20,20 +30,30 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const body = request.body;
+  console.log(body)
+
   return response.json([
-    'Imovel negociado 2021',
-    'Imovel 2 negociado 2021',
+    'Primeiro freella do Brunao',
+    'Segundo freella do Brunao',
     'Compra do ape pro meu pai'
   ]);
 });
 
 app.put('/projects/:id', (request, response) => {
+  const params = request.params;
+  console.log(params);
+
   return response.json([
     'freella3',
     'freella4',
     'freella5'
    ]);
 })
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json
+});
 
 app.listen(3333, () => {
   console.log('Backend STARTED!*!*!*')
