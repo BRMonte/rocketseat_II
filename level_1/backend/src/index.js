86fe80e2ccc7bd63bd7eb564e901aba03985ac51
Bruno Montebello <brunormontebello@gmail.com>
2020-12-16 22:06:08 +0000

    Create routes + add nodemon

--- level_1/backend/src/index.js
+++ level_1/backend/src/index.js
@@ -1,4 +1,40 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 
-app.listen(3333);
+/**
+ * METODOS HTTP:
+ * GET: Buscar infos no backend
+ * POST: criar infos no backend
+ * PATCH: atualiza uma info especifica
+ * PUT: atualiza todos os dados de um recurso (EXEMPLO DE RECURSO:/projects)
+ * DELETE: deletar infos no
+ *
+ */
+
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Primeiro freella do Brunao',
+    'Segundo freella do Brunao'
+   ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Imovel negociado 2021',
+    'Imovel 2 negociado 2021',
+    'Compra do ape pro meu pai'
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'freella3',
+    'freella4',
+    'freella5'
+   ]);
+})
+
+app.listen(3333, () => {
+  console.log('Backend STARTED!*!*!*')
+});
