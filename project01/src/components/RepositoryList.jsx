cfdcb79bc7d11ab9451d7eda9603eab1970c06c3
Bruno Montebello <brunormontebello@hotmail.com>
2021-03-15 11:04:36 +0000

    Create repositoryList component

---
+++ project01/src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+const RepositoryList = () => {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositorios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Form in react</p>
+          <a href="#">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Form in react</p>
+          <a href="#">Acessar repositorio</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Form in react</p>
+          <a href="#">Acessar repositorio</a>
+        </li>
+      </ul>
+    </section>
+  )
+};
+
+export default RepositoryList;
